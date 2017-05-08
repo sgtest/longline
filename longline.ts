596f2f244a835dd2f637f5c152f0a4a97f5f1d65
Quinn Slack <qslack@qslack.com>
2017-05-07 21:02:40 -0700

    Create longline.ts

---
+++ longline.ts
@@ -0,0 +1,1 @@
+if (!this.configurationService.getConfiguration<IDebugConfiguration>('debug').inlineValues || !model || !stackFrame || model.uri.toString() !== stackFrame.source.uri.toString() || !this.configurationService.getConfiguration<IDebugConfiguration>('debug').inlineValues || !model || !stackFrame || model.uri.toString() !== stackFrame.source.uri.toString()) {
